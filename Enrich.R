da1012716494570dc6fb9c7415f5c9ce0744feaa
lengning <lengning1@gmail.com>
2015-06-10 10:55:19 -0500

    Enrichment test
    using PCA loadings

---
+++ Enrich.R
@@ -0,0 +1,59 @@
+
+options <- commandArgs(trailingOnly = TRUE)
+print(options)
+File=options[1] # score file, rows are genes
+Local=options[2] # local list; rows are lists
+if(length(options)<2)Local=NULL
+
+
+# csv or txt
+tmp=strsplit(File, split="\\.")[[1]]
+FileType=tmp[length(tmp)]
+
+if(FileType=="csv"){
+	cat("\n Read in csv file \n")
+	prefix=strsplit(File,split="\\.csv")[[1]][1]
+	In=read.csv(File,stringsAsFactors=F,row.names=1)
+}
+if(FileType!="csv"){
+	cat("\n Read in tab delimited file \n")
+	prefix=strsplit(File,split=paste0("\\.",FileType))[[1]][1]
+	In=read.table(File,stringsAsFactors=F,row.names=1)
+}
+
+
+
+if(!is.null(Local)){
+	tmp2=strsplit(Local, split="\\.")[[1]]
+	FileType2=tmp2[length(tmp2)]
+
+	if(FileType2=="csv"){
+		cat("\n Read in csv file (gene list)\n")
+		ListIn=read.csv(Local,stringsAsFactors=F,row.names=1)
+	}
+	if(FileType2!="csv"){
+		cat("\n Read in tab delimited file (gene list)\n")
+		ListIn=read.table(Local,stringsAsFactors=F,row.names=1)
+	}
+
+	List=sapply(1:nrow(ListIn),function(i)setdiff(as.vector(ListIn[i,]),c(""," ")))
+	names(List)=rownames(ListIn)
+
+} else List=NULL
+
+library(allez)
+Score=In[[1]]
+names(Score)=rownames(In)
+Out=allez(score=Score,lib="org.Hs.eg",idtype="SYMBOL",locallist=List)
+
+Mat=Out$setscores[,c("Term","set.mean","set.sd","set.size","z.score")]
+Mat$p.value <- pnorm(-abs(Mat$z.score))
+Mat$p.adj <- p.adjust(Mat$p.value, method="BH")
+MatOut=Mat[order(Mat$p.value),c("Term","p.value","p.adj","z.score","set.size","set.mean","set.sd")]
+
+LocalOut=MatOut[which(is.na(MatOut[,"Term"])),]
+write.csv(MatOut,file=paste0(prefix,"_enrichment_allsets.csv"))
+write.csv(LocalOut,file=paste0(prefix,"_enrichment_localsets.csv"))
+
+
+
